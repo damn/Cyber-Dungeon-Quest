f9f83a05678915220651414d336b6db8e0558e6e
Michael Sappler <michaelsappler@gmail.com>
2023-11-26 12:10:28 +0100

    Remove comments, dead code

--- src/game/render_ingame.clj
+++ src/game/render_ingame.clj
@@ -1,4 +1,3 @@
-; TODO move to screens/ingame ( also update-ingame)
 (ns game.render-ingame
   (:require [gdl.graphics.world :as world]
             [gdl.graphics.gui :as gui]
@@ -9,10 +8,8 @@
             [game.line-of-sight :refer (in-line-of-sight?)]
             [game.maps.data :refer [get-current-map-data]]
             [game.render :as render]
-            ; Ingame GUI render fns
             [game.player.status-gui :refer [render-player-hp-mana]]
             [game.utils.msg-to-player :refer [render-message-to-player]]
-            ; Ingame GUI render fns
             [game.update-ingame :refer (thrown-error)]
             [game.player.entity :refer (player-entity)]
             [game.maps.contentfields :refer [get-entities-in-active-content-fields]]
@@ -23,11 +20,9 @@
         radius 0.8
         circle {:position position :radius radius}]
     (shape-drawer/circle position radius (color/rgb 1 0 0 0.5))
-
     (doseq [[x y] (map #(:position @%)
                        (@#'game.maps.cell-grid/circle->touched-cells circle))]
       (shape-drawer/rectangle x y 1 1 (color/rgb 1 0 0 0.5)))
-
     (let [{[x y] :left-bottom :keys [width height]} (gdl.geom/circle->outer-rectangle circle)]
       (shape-drawer/rectangle x y width height (color/rgb 0 0 1 1)))))
 
@@ -49,7 +44,6 @@
                        1
                        1
                        (color/rgb 0.5 0.5 0.5 0.5))
-
     (doseq [[x y] (world/visible-tiles)
             :let [cell (game.maps.cell-grid/get-cell [x y])
                   faction :good
@@ -59,24 +53,17 @@
                                 (if blocked?
                                   color/red
                                   color/green))
-
       (let [ratio (/ (int (/ distance 10)) 15)]
         (shape-drawer/filled-rectangle x y 1 1
                                        (color/rgb ratio (- 1 ratio) ratio 0.6)))
-
       #_(@#'g/draw-string x y (str distance) 1)
-
       #_(when (:monster @cell)
           (@#'g/draw-string x y (str (:id @(:monster @cell))) 1)))))
 
 (defn- render-map-content []
-  #_(tile-debug) ; TODO make debug options for everything. (dear-imgui ?)
-
+  #_(tile-debug)
   (render/render-entities* (visible-entities*))
-
   #_(geom-test)
-
-
   ; highlight current mouseover-tile
   #_(let [[x y] (mapv int (world/mouse-position))
           cell (game.maps.cell-grid/get-cell [x y])]
@@ -91,29 +78,10 @@
   (let [[tile-x tile-y] (world/mouse-position)]
     (str (float tile-x) " " (float tile-y))))
 
-; y selbst aufbauen mit font height.
-#_(defn- render-debug [x mouseover-body]
-  (let [starty 30
-        text nil
-        lineh (get-text-height text)]
-
-    (render-readable-text 0 0 {} (str (get-fps)))
-
-    (render-readable-text x (+ starty (* lineh 0)) {} (str "mouse" (get-mouse-pos)))
-
-    (when debug/show-contentfield
-      (render-readable-text x (+ starty (* lineh 1)) {} (str "player content field:"  (get-player-content-field-idx))))
-
-    (when debug/show-float-mouse-pos
-      (render-readable-text x (+ starty (* lineh 3)) {} (str "maus-tile x,y = "  (print-mouse-tile-position))))
-
-    (when debug/show-tile-mouse-pos
-      (render-readable-text x (+ starty (* lineh 4)) {} (str "int-tile x,y = "  (mouse-int-tile-pos))))))
-
-; TODO do everything with scene2d
+; TODO use scene2d
 (defn- render-gui []
   (render-player-hp-mana)
-  (render-message-to-player)) ; TODO render over gui windows... in extra window @ stage ?? top lvl ?
+  (render-message-to-player))
 
 (defn render-game []
   (tiled/render-map (:tiled-map (get-current-map-data))
