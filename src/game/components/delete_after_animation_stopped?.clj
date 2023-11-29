227b048e7cf4183051ccb16da9eb40bec4de27b5
Michael Sappler <michaelsappler@gmail.com>
2023-11-29 15:58:44 +0100

    Add assert to delete-after-animation-stopped?

--- src/game/components/delete_after_animation_stopped?.clj
+++ src/game/components/delete_after_animation_stopped?.clj
@@ -1,9 +1,12 @@
 (ns game.components.delete-after-animation-stopped?
   (:require [x.x :refer [defcomponent]]
             [gdl.graphics.animation :as animation]
+            [game.db :as db]
             [game.tick :refer [tick!]]))
 
 (defcomponent :delete-after-animation-stopped? _
+  (db/create! [_ e]
+    (-> @e :animation :looping not assert))
   (tick! [_ e delta]
     (when (-> @e :animation animation/stopped?)
       (swap! e assoc :destroyed? true))))
