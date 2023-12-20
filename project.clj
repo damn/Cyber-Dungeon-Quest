0b794a90bcdc8671f827aadf7a8ef89e5d7e7f23
Michael Sappler <michaelsappler@gmail.com>
2023-12-20 22:24:49 +0100

    Add lein hiera comment

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
                  [com.github.damn/grid2d "1.0"]
                  [com.github.damn/gdl "main-SNAPSHOT"]
                  [com.github.damn/x.x "195784a"]
-                 [reduce-fsm "0.1.4"]]
+                 [reduce-fsm "0.1.4"]
+                 [lein-hiera "2.0.0"]
+                 ]
   :plugins [[jonase/eastwood "1.2.2"]
             [lein-ancient "1.0.0-RC3"]
             [lein-codox "0.10.8"]
@@ -38,3 +40,22 @@
 ; -> report to vim fireplace?
 
 ; :FireplaceConnect 7888
+
+(comment
+ ; https://github.com/greglook/clj-hiera/blob/main/src/hiera/main.clj
+ (require '[hiera.main :as hiera])
+ (hiera/main-)
+
+ (hiera/graph
+  {:sources #{"src"}
+   :output "target/hiera"
+   :layout :horizontal
+   :cluster-depth 3
+   :external false
+   :ignore #{"data" "utils" "mapgen" "screens" "app"
+             "game.context"
+             "game.effect"
+             "game.modifier"
+             }}
+  )
+ )
