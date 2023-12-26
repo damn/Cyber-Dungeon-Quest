e9007ebb58b21c13e2868dd8ca9bef0894ebde73
Michael Sappler <michaelsappler@gmail.com>
2023-12-26 12:56:07 +0100

    Clean up context.cursor

--- src/context/cursor.clj
+++ src/context/cursor.clj
@@ -8,20 +8,23 @@
   (set-cursor! [{:keys [context/cursors] :as ctx} cursor-key]
     (gdl.context/set-cursor! ctx (safe-get cursors cursor-key))))
 
+(def ^:private cursors
+  {:cursors/default ["default" 0 0]
+   :cursors/black-x ["black_x" 0 0]
+   :cursors/denied ["denied" 16 16]
+   :cursors/hand-grab ["hand003" 4 16]
+   :cursors/hand-before-grab ["hand004" 4 16]
+   :cursors/over-button ["hand002" 0 0]
+   :cursors/sandclock ["sandclock" 16 16]
+   :cursors/walking ["walking" 16 16]
+   :cursors/no-skill-selected ["denied003" 0 0]
+   :cursors/use-skill ["pointer004" 0 0]
+   :cursors/skill-not-usable ["x007" 0 0]
+   :cursors/bag ["bag001" 0 0]
+   :cursors/move-window ["move002" 16 16]})
+
 ; TODO dispose cursors ( do @ gdl ? )
 (defn ->context [ctx]
-  {:context/cursors (->> {:cursors/default ["default" 0 0]
-                          :cursors/black-x ["black_x" 0 0]
-                          :cursors/denied ["denied" 16 16]
-                          :cursors/hand-grab ["hand003" 4 16]
-                          :cursors/hand-before-grab ["hand004" 4 16]
-                          :cursors/over-button ["hand002" 0 0]
-                          :cursors/sandclock ["sandclock" 16 16]
-                          :cursors/walking ["walking" 16 16]
-                          :cursors/no-skill-selected ["denied003" 0 0]
-                          :cursors/use-skill ["pointer004" 0 0]
-                          :cursors/skill-not-usable ["x007" 0 0]
-                          :cursors/bag ["bag001" 0 0]
-                          :cursors/move-window ["move002" 16 16]}
-                         (mapvals (fn [[file x y]]
-                                    (->cursor ctx (str "cursors/" file ".png") x y))))})
+  {:context/cursors (mapvals (fn [[file x y]]
+                               (->cursor ctx (str "cursors/" file ".png") x y))
+                             cursors)})
