7e80fbf6738b335bd48442d0390e4ba5d4556527
Michael Sappler <michaelsappler@gmail.com>
2024-01-01 02:10:46 +0100

    Align skill infotext

--- src/context/skill_info.clj
+++ src/context/skill_info.clj
@@ -8,16 +8,11 @@
 (extend-type gdl.context.Context
   cdq.context/SkillInfo
   (skill-text [{:keys [context/player-entity] :as context}
-               {:keys [id
-                       cost
-                       action-time
-                       cooldown
-                       spell?
-                       effect]}]
-              (str (str/capitalize (name id)) "\n"
-                   (if spell? "Spell" "Weapon") "\n"
-                   (when cost (str "Cost " cost  "\n"))
-                   (if spell?  "Cast-Time " "Attack-time ") (readable-number action-time) " seconds\n"
-                   (when cooldown (str "Cooldown " (readable-number cooldown) "\n"))
-                   (effect-text (merge context {:effect/source player-entity})
-                                effect))))
+               {:keys [id cost action-time cooldown spell? effect]}]
+    (str (str/capitalize (name id)) "\n"
+         (if spell? "Spell" "Weapon") "\n"
+         (when cost (str "Cost " cost  "\n"))
+         (if spell?  "Cast-Time " "Attack-time ") (readable-number action-time) " seconds\n"
+         (when cooldown (str "Cooldown " (readable-number cooldown) "\n"))
+         (effect-text (merge context {:effect/source player-entity})
+                      effect))))
