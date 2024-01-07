51361eeee90c076b3e16301c44061483073c7417
Michael Sappler <michaelsappler@gmail.com>
2024-01-07 14:12:51 +0100

    Remove comments at modifier

--- src/cdq/context/modifier.clj
+++ src/cdq/context/modifier.clj
@@ -9,10 +9,6 @@
   (alter-var-root #'modifier-definitions assoc k modifier)
   k)
 
-; TODO assert, do like effect
-
-; Example:
-; (call-modifier-fn :apply entity* [:mana 30])
 (defn- call-modifier-fn [fn-k entity* [modifier-key modifier-value]]
   {:pre [(#{:apply :reverse} fn-k)]}
   (let [modifier-def (get modifier-definitions modifier-key)
@@ -31,29 +27,6 @@
 (defn- text [[modifier-type value]]
   ((:text (modifier-type modifier-definitions))
    value))
-
-; TODO modifiers without reduce
-; => just effect with an reverse! function
-; calling doseq ! ?
-; but I like reduce & simple function
-; why does everything have to be with side effects ?
-; weil man darauf reagieren muss im falle hp
-; im falle mana vlt auch , flash manabar
-; speed/damage/block modifier idk
-; could flash the stats labels in stats window below inventory
-; => a way to organise/data based define swap! entity 'effects'
-; can I reuse this in other swap entity effects ?
-; [:mark-for-removal! entity]
-; data based side effect language ? possible ?
-; where all are these ctx fns called
-; set/remove item is a 'modifier' itself o.o
-
-; => don't reduce two reasons:
-; * I can REACT to it
-; * permanent blessings/curses which show small icon somewhere or component drawing with tooltip which will
-; not be reversed, => use them as effects
-; e.g. curse your opponents => dark energy => -50% HP, +50% strength.
-; * similar to effects - text ...
 
 (defmethod cdq.context/transact! :tx/apply-modifier [[_ entity modifier] ctx]
   [(apply-modifier @entity modifier)])
