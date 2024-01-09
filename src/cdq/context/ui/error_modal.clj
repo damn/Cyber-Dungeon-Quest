e39d555b40adfffa66b9f7428ee0e7ea2694b1f6
Michael Sappler <michaelsappler@gmail.com>
2024-01-09 16:05:09 +0100

    Fix add-to-stage!

--- src/cdq/context/ui/error_modal.clj
+++ src/cdq/context/ui/error_modal.clj
@@ -5,11 +5,11 @@
 (extend-type gdl.context.Context
   cdq.context/ErrorModal
   (->error-window [ctx throwable]
-    (add-to-stage!
-     (->window ctx {:title "Error"
-                    :rows [[(->label ctx (str throwable))]]
-                    :modal? true
-                    :close-button? true
-                    :close-on-escape? true
-                    :center? true
-                    :pack? true}))))
+    (add-to-stage! ctx
+                   (->window ctx {:title "Error"
+                                  :rows [[(->label ctx (str throwable))]]
+                                  :modal? true
+                                  :close-button? true
+                                  :close-on-escape? true
+                                  :center? true
+                                  :pack? true}))))
