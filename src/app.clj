886c96311e2c6d7065d0dad0f2428e228bb51c33
Michael Sappler <michaelsappler@gmail.com>
2024-07-29 12:17:03 +0530

    context/config

--- src/app.clj
+++ src/app.clj
@@ -1,33 +1,19 @@
 (ns app
-  (:require [core.component :as component]
-            [gdl.context :as ctx]
-            [gdl.backends.libgdx.app :as app]
-            (cdq.context [properties :as properties]
-                         [cursor :as cursor]
+  (:require [gdl.backends.libgdx.app :as app]
+            (cdq.context properties
+                         cursor
                          builder
+                         config
                          effect
                          modifier
                          potential-fields
                          render-debug
-                         [screens :as screens]
+                         screens
                          transaction-handler
-                         [action-bar :as action-bar]
-                         [inventory-window :as inventory-window])
+                         action-bar
+                         inventory-window)
             (cdq.context.ui player-modal
                             error-modal)))
-
-(component/def :context/config {}
-  tag
-  (ctx/create [_ _ctx]
-    (case tag
-      :prod {:map-editor? false
-             :property-editor? false
-             :debug-windows? false
-             :debug-options? false}
-      :dev {:map-editor? true
-            :property-editor? true
-            :debug-windows? true
-            :debug-options? true})))
 
 (def ^:private app-config
   {:app {:title "Cyber Dungeon Quest"
@@ -43,7 +29,15 @@
              [:context/cursors true]
              [:context/inventory true]
              [:cdq.context.action-bar/data true]
-             [:context/config :dev]
+             [:context/config {:tag :dev
+                               :configs {:prod {:map-editor? false
+                                                :property-editor? false
+                                                :debug-windows? false
+                                                :debug-options? false}
+                                         :dev {:map-editor? true
+                                               :property-editor? true
+                                               :debug-windows? true
+                                               :debug-options? true}}}]
              ; requires context/config (debug-windows)
              ; make asserts .... for all dependencies ... everywhere o.o
              [:context/screens true]]})
